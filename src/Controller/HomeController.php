26e1ec3e8e1e8d0f3cfab42d8920846eee3abe06
Jordan2802 <39262332+Jordan2802@users.noreply.github.com>
2020-02-26 14:09:27 +0100

    gestion des utilisateur et gestion avec les annonces

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,23 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController{
 
-    /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     * @Route("/bonjourtest", name="hello_base")
-     * montre la page qui dit bonjour
-     *
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0) {
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom' => $prenom,
-                'age' => $age
-            ]
-            );
-    }
+  
 
     /**
      * @Route("/", name="homepage")
