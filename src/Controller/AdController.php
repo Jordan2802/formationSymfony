8a07d32f0272fddaa9e97ab71386cec689ba5fba
Jordan2802 <39262332+Jordan2802@users.noreply.github.com>
2020-02-26 15:16:36 +0100

    ajour de la logique des roles et de la sécurité

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -28,7 +30,11 @@ class AdController extends AbstractController
 
     /**
      * permet de créer une annonce
+     * 
      *@Route("/ads/new", name="ads_create")
+     *
+     *@IsGranted("ROLE_USER")
+     * 
      * @return Response
      */
     public function create(Request $request, EntityManagerInterface $manager) {
@@ -70,7 +76,10 @@ class AdController extends AbstractController
 
     /**
      * Permet d'afficher le formulaire d'édition
+     * 
      * @Route("/ads/{slug}/edit", name="ads_edit")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas, vous ne pouvez pas la modifier")
+     * 
      * @return response
      */
     public function edit(Ad $ad, Request $request, EntityManagerInterface $manager){
@@ -121,4 +130,26 @@ class AdController extends AbstractController
     }
 
 
+
+    /**
+     * Permet de supprimer une annonce
+     * 
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Vous n'avez pas le droit de supprimer cette annonce")
+     *
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, EntityManagerInterface $manager){
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> ont bien été supprimée !"
+        );
+
+        return $this->redirectToRoute("ads_index");
+    }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Bluemmb\Faker\PicsumPhotosProvider;
@@ -22,6 +23,26 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('FR-fr');
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Jordan')
+                  ->setLastName('Oudot')
+                  ->setEmail('jordan@symfony.com')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setPicture('https://twitter.com/Rhudra/photo')
+                  ->setIntroduction($faker->sentence())
+                  ->setDescription('<p>'. join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                  ->addUserRole($adminRole);
+
+        $manager->persist($adminUser);
+
+
+
+
 
 
         //Nous gérons les utilisateurs
