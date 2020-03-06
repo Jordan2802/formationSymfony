3f640749668febc63a2c968fde0402b3c94028fc
Jordan2802 <39262332+Jordan2802@users.noreply.github.com>
2020-03-06 16:37:40 +0100

    administration du site

---
+++ src/Controller/AdminBookingController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminBookingController extends AbstractController
+{
+    /**
+     * @Route("/admin/bookings", name="admin_booking_index")
+     */
+    public function index(BookingRepository $repo)
+    {
+        return $this->render('admin/booking/index.html.twig', [
+            'bookings' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet de modifier une réservation
+     *
+     * @Route("admin/bookings/{id}/edit", name="admin_booking_edit")
+     * 
+     * @param Booking $booking
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Booking $booking, Request $request, EntityManagerInterface $manager) {
+
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $booking->setAmount(0);
+            
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "la réservation numéro <strong> {$booking->getId()} </strong> a bien été enregistrée !"
+            );
+            return $this->redirectToRoute("admin_booking_index");
+        }
+
+        return $this->render('admin/booking/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de supprimer une réservation
+     * 
+     * @Route("/admin/bookings/{id}/delete", name="admin_booking_delete")
+     *
+     * @param Booking $booking
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function delete(Booking $booking, EntityManagerInterface $manager) {
+
+            $manager->remove($booking);
+            $manager->flush();
+    
+            $this->addFlash(
+                'success',
+                "La réservation de <strong> {$booking->getBooker()->getFullName()} </strong> a bien été supprimée !"
+            );
+        
+        return $this->redirectToRoute('admin_booking_index');
+    }
+}
